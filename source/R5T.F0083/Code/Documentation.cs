6916506b039da1acff5892bf39e76d31c3682398
davidcoats <david@getsafetycone.com>
2022-11-20 23:42:40 -0800

    Initial commit.

---
+++ source/R5T.F0083/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0083
+{
+	/// <summary>
+	/// Code file generation operations with Razor components.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.F0083/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.F0083
+{
+    public static class Instances
+    {
+		
+    }
+}
